eeef31119075ea7449e80c484ae8ce62a351d877
fedirky <fedirky@gmail.com>
2025-03-02 02:06:18 +0200

    fixed all errors

--- color_rect_light_rays.gd
+++ color_rect_light_rays.gd
@@ -5,8 +5,22 @@ extends ColorRect
 
 
 func _ready() -> void:
+    # Встановлюємо ColorRect вручну на 2560x1440
     set_anchors_preset(Control.PRESET_FULL_RECT)
-
+    anchor_left = 0.0
+    anchor_right = 0.0
+    anchor_top = 0.0
+    anchor_bottom = 0.0
+    offset_left = 0
+    offset_right = 2560  # Ширина
+    offset_top = 0
+    offset_bottom = 1440  # Висота
+    
+    # Додаємо тестовий білий фон
+    color = Color(1, 1, 1, 1)
+    visible = true
+    
+    print("ColorRect size:", size)  # Перевіряємо правильність розміру
 
 func _process(_delta: float) -> void:
     var pos = camera.unproject_position(camera.global_position - (-light.global_basis.z.normalized()))   
